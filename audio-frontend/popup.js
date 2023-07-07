153191493837226a23af3f4c16b370ccd11347c3
Elston <elston1703@gmail.com>
2023-07-07 12:17:08 +0800

    first commit

---
+++ audio-frontend/popup.js
@@ -0,0 +1,145 @@
+showLatestTranscript();
+chrome.storage.local.get("transcript", ({ transcript }) => {
+  if (transcript && transcript !== "") {
+    document.getElementById("stop").disabled = false;
+    document.getElementById("start").disabled = true;
+  }
+});
+getCurrentTab().then((tab) => {
+  window.elliottTab = tab;
+});
+
+document.getElementById("stop").addEventListener("click", async () => {
+  document.getElementById("stop").disabled = true;
+  document.getElementById("start").disabled = false;
+  const tab = await getCurrentTab();
+  if (!tab) return alert("Require an active tab");
+  chrome.tabs.sendMessage(tab.id, { message: "stop" });
+});
+
+document.getElementById("clear").addEventListener("click", async () => {
+  const res = await fetch("http://localhost:5000/clear", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      question: input,
+      url: window.elliottTab.url,
+    }),
+  });
+});
+
+document.getElementById("scribe").addEventListener("click", async () => {
+  document.getElementById("stop").disabled = true;
+  document.getElementById("scribe").disabled = true;
+  fetch("http://localhost:5000/scribe", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    url: window.elliottTab.url,
+  }),
+})
+  .then((res) => res.blob())
+  .then((blob) => {
+    const objectURL = URL.createObjectURL(blob);
+    chrome.downloads.download({
+      url: objectURL,
+      filename: "minutes.docx",
+    });
+  })
+  .catch((error) => {
+    console.error("Error:", error);
+  });
+  /*
+  await fetch("http://localhost:5000/scribe", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      url: window.elliottTab.url,
+    }),
+  }).then((res) => {
+    var blob = res.blob(type="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+    //var blob = new Blob(res, { type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document" });
+    const objectURL = URL.createObjectURL(blob);
+    chrome.downloads.download({
+      url: objectURL,
+      //filename: "./minutes.docx"
+    });
+  });
+  */
+});
+
+document.getElementById("start").addEventListener("click", async () => {
+  const tab = await getCurrentTab();
+  document.getElementById("stop").disabled = false;
+  document.getElementById("start").disabled = true;
+  if (!tab) return alert("Require an active tab");
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    files: ["main.js"],
+  });
+});
+
+async function getCurrentTab() {
+  const queryOptions = { active: true, lastFocusedWindow: true };
+  const [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+chrome.runtime.onMessage.addListener(({ message }) => {
+  if (message == "transcriptavailable") {
+    console.log(showLatestTranscript());
+  }
+});
+
+chrome.runtime.onMessage.addListener(({ message }) => {
+  if (message == "stop") {
+    socket.close();
+    alert("Transcription ended");
+    console.log("message", message);
+  }
+});
+
+function showLatestTranscript() {
+  chrome.storage.local.get("transcript", ({ transcript }) => {
+    // document.getElementById("transcript").innerHTML = transcript;
+  });
+}
+
+async function sendQuestion(e) {
+  e.preventDefault();
+  const input = document.getElementById("question-input").value;
+  const res = await fetch("http://localhost:5000/questioning", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      question: input,
+      url: window.elliottTab.url,
+    }),
+  });
+  const data = await res.json();
+  const qnaDiv = document.getElementById("qna");
+  // <div style="margin-top: 1rem">
+  //       <p id="question">Q: <strong>What is this about?</strong></p>
+  //       <p id="answer">Reluvape: <strong>this is about you</strong></p>
+  //     </div>
+  const div = document.createElement("div");
+  div.style.marginTop = "1rem";
+  const question = document.createElement("p");
+  question.id = "question";
+  question.innerHTML = `Q: <strong>${input}</strong>`;
+  const answer = document.createElement("p");
+  answer.id = "answer";
+  answer.innerHTML = `Reluvape: <strong>${data.answer}</strong>`;
+  div.appendChild(question);
+  div.appendChild(answer);
+  qnaDiv.appendChild(div);
+  document.getElementById("question-input").value = "";
+}
---
+++ backend/backend.py
@@ -0,0 +1,169 @@
+from flask import Flask, render_template, request, session, send_file, send_from_directory
+from flask_cors import CORS, cross_origin
+import ai21
+from docx import Document
+from docx.shared import Inches
+import requests
+import redis
+import os
+
+
+r = redis.Redis(
+    host='redis-14946.c23738.us-east-1-mz.ec2.cloud.rlrcp.com',
+    port=14946,
+    password='g9e5H3aUeAYVOKHnYDMEQoy4QidRElPR')
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+ai21.api_key = "Gv4kFMkfaThv9mH4mVhAPMjJLvFIK9tl"
+
+def get_transcription(url: str) -> str:
+    value = r.get(url)
+    if value is None:
+        return ""
+    else:
+        return value.decode("utf-8")
+    
+def append_transcription(url: str, transcript: str):
+    # if url in r: append to existing
+    # else: create new
+    existing = r.get(url)
+    if existing is None:
+        r.set(url, transcript)
+    else:
+        existing = existing.decode("utf-8")
+        r.set(url, existing + ' ' + transcript)
+
+def ask(transcript, question):
+    headers = {"Authorization": f"Bearer {ai21.api_key}", "Content-Type": "application/json"}
+    payload = {
+        "context": transcript,
+        "question": question
+    }
+    endpoint = "https://api.ai21.com/studio/v1/experimental/answer"
+    try:
+        result = requests.post(endpoint, json = payload, headers=headers)
+        result = result.json()
+        answer = result["answer"]
+        return answer
+    except Exception as e:
+        return {"status" : 0, "error": f"{type(e)} {e}"}
+
+def createDoc(transcript):
+    headers = {"Authorization": f"Bearer {ai21.api_key}", "Content-Type": "application/json"}
+    payload = {
+        "context": transcript,
+        "question": "What are the tasks and people responsible for them?"
+    }
+    endpoint = "https://api.ai21.com/studio/v1/experimental/answer"
+    try:
+        summary = ai21.Summarize.execute(
+            source=transcript,
+            sourceType="TEXT"
+        )
+    except Exception as e:
+        print(e)
+        return {"status" : 0, "error": f"{type(e)} {e}"}
+    
+    summary = summary['summary']
+    document = Document()
+    document.add_heading('Meeting Minutes', 0)
+
+    document.add_heading('Summary', level=1)
+    document.add_paragraph(summary)
+    document.add_paragraph("")
+    answer = ask(transcript, "Give me a summary of the meeting in bullet points")
+    document.add_paragraph(answer)
+
+    document.add_heading('Responsibilities', level=1)
+    answer = ask(transcript, "What are the tasks and people responsible for them?")
+    document.add_paragraph(
+        answer
+    )
+
+    document.save('minutes.docx')
+    return document
+
+@app.route('/scribe', methods=["POST"])
+def scribe():
+    transcript = """
+    So regarding our startup plan, I think we should focus on building
+    the MVP now, the minimal viable product, so Sithu, Timothy and Elliott will work 
+    on the MVP along with Amir, you guys will be handling the web development, security
+    and the database. I will be working on the business plan and the marketing strategy.
+    Dr wilson will be mentoring us and guiding us while giving us advice so if you guys have any questions
+    just go to him. Me and Amir will also talk to the investors and get the funding for the startup as well as
+    companies like AISG, AWS, Google Cloud and Microsoft Azure to partner with them in order to get their specialists
+    to curate content such as blogs and courses for us. Can we get the MVP done by around 10th October 2023? That would be great.
+    Me and Amir will be talking to business partners for at least 2 months and try to get courses so you guys can fill up
+    the course page. I think after getting investment, we will have around 10 thousand dollars to spend 
+    and those 10 thousand dollars could go to us hosting hackathons or in person conferences.
+    Then we will get more exposure and marketing at the same time which will be awesome.
+    """
+    if request.method == "POST":
+        path = "../minutes.docx"
+        url = request.get_json()['url']
+        #transcript = get_transcription(url)
+        print(transcript)
+        document = createDoc(transcript)
+        #return ("OK", 200)
+        #return send_file(path, as_attachment=True)
+        return send_from_directory('../', 'minutes.docx', as_attachment=True)
+    else:
+        pass
+    
+@app.route('/clear', methods=["POST"])
+def clear():
+    if request.method == "POST":
+        url = request.get_json()['url']
+        r.delete(url)
+        return ("OK", 200)
+    else:
+        pass
+
+@app.route('/summarize', methods=["POST"])
+def summarise():
+    if request.method == "POST":
+        #data = request.data
+        #data = data.decode("utf-8")
+        # data = eval(data) #data should only have one argument
+        url = request.get_json()['url']
+        transcript = get_transcription(url)
+        try:
+            summary = ai21.Summarize.execute(
+                source=transcript,
+                sourceType="TEXT"
+            )
+        except Exception as e:
+            return (str(e), 400)
+
+        return ({'summary':summary["summary"]}, 200)
+
+    else:
+        pass
+
+
+@app.route('/append-transcript', methods=["POST"])
+def appendTranscript():
+    if request.method == 'POST':
+        body = request.get_json()
+        if 'url' in body and 'stream' in body:
+            append_transcription(body['url'], body['stream'])
+            return ('OK', 200)
+        else:
+            return ('Bad Request', 400)
+
+@app.route('/get-transcript', methods=["POST"])
+def getTranscript():
+    if request.method == 'POST':
+        body = request.get_json()
+        if 'url' in body:
+            value = get_transcription(body['url'])
+            return (value, 200)
+        else:
+            return ('Bad Request', 400)
+
+
+if __name__ == '__main__':
+    # conn_dict = psycopg.conninfo.conninfo_to_dict(connection)
+    app.run(debug=True)
